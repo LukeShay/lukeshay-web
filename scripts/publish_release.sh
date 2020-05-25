f464c87f92d87f9d6ebe21aeccdb827aded59b25
Luke Shay <shay.luke17@gmail.com>
2020-05-25 17:12:44 -0500

    Fixed.

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -23,4 +23,13 @@ git checkout -b ${BRANCH_NAME}
 git acm ${COMMIT}
 git push --set-upstream origin ${BRANCH_NAME}
 
+for D in `find tools -type d -maxdepth 1`
+do
+  cd "${D}"
+  V=$(npm view . version)
+  git tag "@lukeshay/${D:5:}_${V}"
+done
+
+git push --tags
+
 gh pr create -t ${COMMIT} -b ${PR_DESC} -w
