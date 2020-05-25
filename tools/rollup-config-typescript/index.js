684ad540f26744e7b870d126c122d37b6d103a1d
Luke Shay <shay.luke17@gmail.com>
2020-05-25 16:57:00 -0500

    Finished config.

--- tools/rollup-config-typescript/index.js
+++ tools/rollup-config-typescript/index.js
@@ -5,7 +5,40 @@ import commonjs from '@rollup/plugin-commonjs';
 import strip from '@rollup/plugin-strip';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
+export function get_config(pkg, input, cjs, es, umd, iife) {
+  var config = {
+    input,
+    output: [],
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      terser(),
+      resolve(),
+      commonjs(),
+      strip(),
+    ],
+  };
+
+  if (cjs) {
+    config.output.append({ file: pkg.cjs, format: 'cjs' });
+  }
+
+  if (es) {
+    config.output.append({ file: pkg.es, format: 'es' });
+  }
+
+  if (umd) {
+    config.output.append({ file: pkg.umd, format: 'umd' });
+  }
+
+  if (iife) {
+    config.output.append({ file: pkg.iife, format: 'iife' });
+  }
+}
+
+export const config = {
   input: 'src/main.ts',
   output: [
     {
