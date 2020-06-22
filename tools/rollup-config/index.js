f730d245b9974346203ea6f1f0ec406e10cec45d
Luke Shay <shay.luke17@gmail.com>
2020-06-21 23:37:21 -0500

    fix reexport

--- tools/rollup-config/index.js
+++ tools/rollup-config/index.js
@@ -3,8 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import strip from '@rollup/plugin-strip';
 import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
-export { typescript } from 'rollup-plugin-typescript2';
+export { default as typescript } from 'rollup-plugin-typescript2';
 
 export const createRollupConfig = (pkg, plugins = []) => {
   const rollupConfig = {
