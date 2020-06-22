136a780cdac322c3205f7455067101ba6b0bdc4f
Luke Shay <shay.luke17@gmail.com>
2020-06-21 23:11:48 -0500

    changed config functions

--- tools/rollup-config/index.js
+++ tools/rollup-config/index.js
@@ -3,32 +3,25 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import strip from '@rollup/plugin-strip';
 import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
 
-export const javascriptConfig = (pkg) => {
-  const config = {
+export { typescript } from 'rollup-plugin-typescript2';
+
+export const createRollupConfig = (pkg, plugins) => {
+  const rollupConfig = {
     input: pkg.input,
     output: [],
     external: [...Object.keys(pkg.dependencies || {})],
-    plugins: [terser(), resolve(), commonjs(), strip(), auto()],
+    plugins: [auto(), commonjs(), resolve(), strip(), terser(), ...plugins],
   };
 
-  if (pkg.cjs) config.output.push({ file: pkg.cjs, format: 'cjs' });
-  if (pkg.esm) config.output.push({ file: pkg.esm, format: 'esm' });
-  if (pkg.umd) config.output.push({ file: pkg.umd, format: 'umd' });
-  if (pkg.iife) config.output.push({ file: pkg.iife, format: 'iife' });
+  if (pkg.cjs) rollupConfig.output.push({ file: pkg.cjs, format: 'cjs' });
+  if (pkg.esm) rollupConfig.output.push({ file: pkg.esm, format: 'esm' });
+  if (pkg.umd) rollupConfig.output.push({ file: pkg.umd, format: 'umd' });
+  if (pkg.iife) rollupConfig.output.push({ file: pkg.iife, format: 'iife' });
 
-  return config;
+  return rollupConfig;
 };
 
-export const typescriptConfig = (pkg) => {
-  const config = javascriptConfig(pkg);
-
-  config.plugins.push(
-    typescript({
-      tsconfig: './tsconfig.json',
-    })
-  );
-
-  return config;
+export const createTypescriptRollupConfig = (pkg) => {
+  return createRollupConfig(pkg, [typescript({ tsconfig: './tsconfig.json' })]);
 };
